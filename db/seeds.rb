973fb856db82ed042e4dcd01d850dce2020d52ed
jjacobsmeyer <jamesjacobsmeyer@gmail.com>
2015-09-03 11:29:28 -0500

    Checkpoint 6 completed, Seeding Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 admin = User.new(
   username:       'Admin User',
   email:      'admin@example.com',
@@ -23,4 +25,24 @@ standard.save!
 # )
 # premium.skip_confirmation!
 # premium.save!
+
+10.times do
+  user = User.new(
+  username: Faker::Name.name,
+  email: Faker::Internet.email,
+  password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+
+100.times do
+  Wiki.create!(
+  title: Faker::Lorem.sentence,
+  body: Faker::Lorem.paragraph
+  )
+end
+
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
 puts "Seed finished"
